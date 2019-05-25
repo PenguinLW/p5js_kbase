58a7591ee4ce1f3c45c2f3fd87d34eadf1a2637e
PenguinL <diana1997525@gmail.com>
2019-05-25 22:46:32 +0800

    Update sketch.js

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -100,6 +100,7 @@ function initWL() {
         for(let i = 0; i <= links.length-20-1; i++) {
           wlinks += "https://kovalsky95.github.io/p5js_kbase/resources/"+(i+1)+".jpg\n";
         }
+        console.log(wlinks);
         wlinks = wlinks.split("\n");
         for(let i = 0; i <= links.length-20-1; i++) {
           wlinks[i] = sk.loadImage(wlinks[i]);
