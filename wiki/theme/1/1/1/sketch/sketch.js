9f5c0bf26b31bdb9c4d581a77d30988d9d73ea04
PenguinL <diana1997525@gmail.com>
2019-06-05 01:23:41 +0800

    Update sketch.js

--- wiki/theme/1/1/1/sketch/sketch.js
+++ wiki/theme/1/1/1/sketch/sketch.js
@@ -12,7 +12,9 @@ function setup() {
 }
 function draw() {
 	background(255);
-	text("Ваше первое js-приложение: о том как выполняется, где и почему размещается наш код.. А также как этого достичь.", w/2, spacer);
+	text("Ваше первое js-приложение.", w/2, spacer);
+	text("О том как выполняется, где и почему размещается наш код.. А также как этого достичь.", w/2, spacer*2);
+	
 }
 function windowResized() {
 	w = windowWidth-spacer/2;
