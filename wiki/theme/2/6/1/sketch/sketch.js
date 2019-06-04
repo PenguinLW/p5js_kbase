c4c4b62ea0907afb4c2c959382e6142998823479
PenguinL <diana1997525@gmail.com>
2019-06-05 01:46:56 +0800

    Add files via upload

--- wiki/theme/2/6/1/sketch/sketch.js
+++ wiki/theme/2/6/1/sketch/sketch.js
@@ -1,12 +1,22 @@
+let w, h, spacer;
 function preload() {
-	alert("done.");
+	spacer = 22;
+	w = windowWidth-spacer/2;
+	h = windowHeight-spacer/2;
 }
 function setup() {
-	createCanvas(250, 250);
+	createCanvas(w, h);
+	textSize(spacer);
+	textAlign(CENTER);
+	textStyle(NORMAL);
 }
 function draw() {
+	background(255);
+	text("Ваше первое js-приложение.", w/2, spacer);
+	text("О том как выполняется, где и почему размещается наш код.. А также как этого достичь.", w/2, spacer*2);
 	
 }
 function windowResized() {
-	
+	w = windowWidth-spacer/2;
+	h = windowHeight-spacer/2;
 }
