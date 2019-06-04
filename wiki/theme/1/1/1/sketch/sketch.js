c274fd11723b89017da8b6089fc5d7dd80ca5416
PenguinL <diana1997525@gmail.com>
2019-06-05 01:02:48 +0800

    Update sketch.js

--- wiki/theme/1/1/1/sketch/sketch.js
+++ wiki/theme/1/1/1/sketch/sketch.js
@@ -1,18 +1,18 @@
 let w, h, spacer;
 function preload() {
-	spacer = 25;
+	spacer = 24;
 	w = windowWidth-spacer/2;
 	h = windowHeight-spacer/2;
 }
 function setup() {
 	createCanvas(w, h);
 	textSize(22);
-	textAlign(LEFT);
+	textAlign(CENTER);
 	textStyle(NORMAL);
 }
 function draw() {
 	background(255);
-	text("Ваше первое js-приложение", spacer, spacer);
+	text("Ваше первое js-приложение", w/2, spacer);
 }
 function windowResized() {
 	w = windowWidth-spacer/2;
