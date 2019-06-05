a5b10c25d8ad8e936cce322b2104daebf493d00c
PenguinL <diana1997525@gmail.com>
2019-06-05 12:21:54 +0800

    Update sketch.js

--- wiki/theme/1/1/1/sketch/sketch.js
+++ wiki/theme/1/1/1/sketch/sketch.js
@@ -6,7 +6,10 @@ function preload() {
 	lis = ""+
 		"Ваше первое js-приложение.\n"+
 		"О том как выполняется, где и почему размещается наш код.. А также как этого достичь.\n"+
-		"Создаём файл web-страницы"+
+		"Создаём файл web-страницы содержащий приблизительно следующий исходный код:\n"+
+		"По устоявшимся традициям, в теге \"head\" указаны \"строки-команды\", которые отвечают за подключение к web-странице файлов таблиц стилей (имеет расширение .css), файлов исходного кода javascript (имеет расширение .js)."+
+		"При помощи команды \"<script src=\"../script/my_app.js\"></script>\" мы сумеем подключить к нашей web-странице файл javascript, находящейся в директории \"script\", являющейся параллельной к директории \"index\" с содержащейся в ней web-страницей:\n"+
+		"При ином расположении директорий и файлов в Вашем проекте относительная адресация может отличаться.\n"+
 		"";
 	pic = ""+
 		"https://kovalsky95.github.io/p5js_kbase/resources/wiki/1.jpg\n"+
@@ -26,12 +29,18 @@ function setup() {
 	lis[0] = new TWrite(lis[0], w/2, spacer);
 	lis[1] = new CWrite(lis[1], w-w+spacer, spacer*2);
 	lis[2] = new CIWrite(lis[2], w/2, spacer*3, pic[0], w/2, spacer*3);
+	lis[3] = new CWrite(lis[3], w-w+spacer, spacer*4);
+	lis[4] = new CIWrite(lis[4], w/2, spacer*3, pic[1], w/2, spacer*5);
+	lis[5] = new CWrite(lis[5], w-w+spacer, spacer*6);
 }
 function draw() {
 	background(255);
 	lis[0].show();
 	lis[1].show();
 	lis[2].show();
+	lis[3].show();
+	lis[4].show();
+	lis[5].show();
 }
 function windowResized() {
 	w = windowWidth-spacer/2;
